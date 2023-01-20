2bd7c04415f36c3606b4c49b14d0fae3d37ac37d
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-20 14:35:27 -0600

    refactoring code

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,23 +1,24 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from app import schemas, database, models
+from app import database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from os import environ as env
+from app.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
-SECRET_KEY = env.get('SECRET_KEY')
-ALGORITHM = env.get('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = env.get('ACCESS_TOKEN_EXPIRE_MINUTES')
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({'exp': expire})
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
@@ -25,12 +26,12 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get('user_id')
+        id: str = payload.get("user_id")
 
         if id is None:
             raise credentials_exception
 
-        token_data = schemas.TokenData(id=id)
+        token_data = str(id) if id is not None else None
 
     except JWTError:
         raise credentials_exception
@@ -39,13 +40,13 @@ def verify_access_token(token: str, credentials_exception):
 
 
 # main function to use as a dependency to get the JWT and verify the access
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='Could not validate the credentials',
-        headers={
-            'WWW-Authenticate': 'Bearer'
-        }
+        detail="Could not validate the credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
 
     token = verify_access_token(token, credentials_exception)
---
+++ app/templates/static/update.js
@@ -0,0 +1,59 @@
+window.addEventListener("load", () => {
+  const form = document.getElementById("login");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let email = document.querySelector("input[name=email]").value;
+    let password = document.querySelector("input[name=password]").value;
+    let newUser = document.querySelector("input[name=newUser]");
+    let data = JSON.stringify({ email: email, password: password });
+    // console.log(data);
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Origin": "*",
+    };
+
+    if (newUser.checked) {
+      // TODO: CHANGE HOST
+      fetch("http://localhost/user", {
+        method: "POST",
+        headers: headers,
+        body: data,
+      })
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          } else {
+            window.alert("Email already in use!");
+            return;
+          }
+        })
+        .then(function (data) {
+          if (data) {
+            console.log("New User Created\n", data);
+            window.alert("New User Saved! Logging in...");
+          }
+        });
+    }
+
+    setTimeout(console.log("Logging in..."), 1500);
+    //  TODO: CHANGE HOST
+    fetch("http://localhost/login", {
+      method: "POST",
+      headers: headers,
+      body: data,
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.access_token) {
+          //   console.log(data.access_token);
+          localStorage.setItem("token", data.access_token);
+          window.location.href = "index.html";
+        } else {
+          window.alert("Invalid Login");
+        }
+      });
+  });
+});
