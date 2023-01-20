0fc2ce78696d0a30dbb8d744fda9ac37a5006fe3
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-20 07:15:13 -0600

    F + R all " for concating file to script

--- app/oauth2.py
+++ app/oauth2.py
@@ -6,18 +6,18 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from os import environ as env
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 
-SECRET_KEY = env.get("SECRET_KEY")
-ALGORITHM = env.get("ALGORITHM")
+SECRET_KEY = env.get('SECRET_KEY')
+ALGORITHM = env.get('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = env.get('ACCESS_TOKEN_EXPIRE_MINUTES')
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
@@ -25,7 +25,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
+        id: str = payload.get('user_id')
 
         if id is None:
             raise credentials_exception
@@ -42,9 +42,9 @@ def verify_access_token(token: str, credentials_exception):
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate the credentials",
+        detail='Could not validate the credentials',
         headers={
-            "WWW-Authenticate": "Bearer"
+            'WWW-Authenticate': 'Bearer'
         }
     )
 
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(
-    prefix="/login",
-    tags=["authentication"]
+    prefix='/login',
+    tags=['authentication']
 )
 
 
-@router.post("/", response_model=schemas.Token)
+@router.post('/', response_model=schemas.Token)
 def login(user_credentials: schemas.UserLogin,
           db: Session = Depends(database.get_db)):
 
@@ -17,11 +17,11 @@ def login(user_credentials: schemas.UserLogin,
     # print(user)
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='invalid Credentials')
 
     if not utils.password_verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='invalid Credentials')
 
-    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    access_token = oauth2.create_access_token(data={'user_id': user.id})
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {'access_token': access_token, 'token_type': 'bearer'}
