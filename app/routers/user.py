fb3a297b7f046907f299c904b5f803058e799ccd
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-20 11:38:33 -0600

    updating code to render templates

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,46 +1,50 @@
-from .. import models, utils, schemas
-from fastapi import status, HTTPException, Depends, APIRouter
+from app import models, utils
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import status, HTTPException, Depends, APIRouter, Request
 from sqlalchemy.orm import Session
-from ..database import get_db
-from ..oauth2 import get_current_user
+from app.database import get_db
+from app.oauth2 import get_current_user
+from pathlib import Path
 
 router = APIRouter(
     prefix='/users',
     tags=['users']
 )
 
+BASE_PATH = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(BASE_PATH / 'templates'))
 
-@router.get('/', response_model=schemas.UserOut)
+@router.get('/', response_class=HTMLResponse)
 def get_user(
+        req: Request,
         db: Session = Depends(get_db),
         current_user: int = Depends(get_current_user)):
     user = db.query(models.User).filter(models.User.id == current_user.id).first()
     if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'user with id: {id} was not found.')
-    return user
-    # returns a JSON version of the user for the user page
+       return templates.TemplateResponse("404.html", {"request": req})
+    return templates.TemplateResponse("user.html", {"request": req, "user": user })
 
 
-@router.put('/{id}', response_model=schemas.UserOut)
-def update_user(id: int,
-                user_data: schemas.UserNew,
+@router.put('/{id}', response_class=HTMLResponse)
+def update_user(req: Request,
+                id: int,
                 db: Session = Depends(get_db),
                 current_user: int = Depends(get_current_user)
                 ):
-    # we want to query this post several times
+
+    user_data = req.form()
+    # TODO: get form data into a user object
+
     user_query = db.query(models.User).filter(models.User.id == id)
     user = user_query.first()
 
     if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'user with id: {id} was not found.')
+        return templates.TemplateResponse("404.html", {"request": req})
 
     if id != current_user.id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail=f'Unauthorized to update user other than current user')
-
-    # check to make sure updated email address doesn't already exist in the database
 
     duplicate_check = db.query(models.User).filter(models.User.email == user_data.email).first()
 
@@ -50,15 +54,17 @@ def update_user(id: int,
 
 
     new_password = utils.password_hash(user_data.password)
-    user_data.password = new_password
+    user_data['password'] = new_password
     user_query.update(user_data.dict(), synchronize_session=False)
     db.commit()
 
-    return user_query.first()
+    return templates.TemplateResponse("update.html", {"request": req})
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_new_user(user: schemas.UserNew, db: Session = Depends(get_db)):
+@router.post('/', status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
+def create_new_user(req: Request, db: Session = Depends(get_db)):
+    user = req.form()
+    # TODO: get form data into a user object
     # check first if the user exists
     exists = db.query(models.User).filter(models.User.email == user.email).first()
 
@@ -81,14 +87,16 @@ def create_new_user(user: schemas.UserNew, db: Session = Depends(get_db)):
 
 @router.delete('/')
 def delete_user(
+        req: Request,
         db: Session = Depends(get_db),
         current_user: int = Depends(get_current_user)):
     user_query = db.query(models.User).filter(models.User.id == current_user.id)
     user = user_query.first()
 
     if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'user with id: {id} was not found.')
+         return templates.TemplateResponse("404.html", {"request": req})
 
     user_query.delete(synchronize_session=False)
     db.commit()
+
+    return templates.TemplateResponse("deleted.html", {"request", req})
